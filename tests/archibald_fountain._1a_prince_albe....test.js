9866b07b94978937f07699fc48a1d4678937ddb1
Elly <elly@mg.crane.ai>
2018-11-03 17:40:53 -0400

    added tests/archibald_fountain refactoring

---
+++ tests/archibald_fountain._1a_prince_albe....test.js
@@ -0,0 +1,21 @@
+/* 
+ * Contact: team@fusioncode.ai
+ *
+ *
+ * Project  - archibald fountain. 1a prince albe... UNIT TESTS
+ * Application Client
+ * new cv deploy test
+*/
+
+/* archibald fountain. 1a prince albe... view has Label located at the bottom left of the view, text needs identified */
+it("archibald fountain. 1a prince albe... view has Label located at the bottom left of the view, text needs identified", () => {
+  /* ToDo developer needs to complete unit test 
+               archibald fountain. 1a prince albe... view has Label located at the bottom left of the view, text needs identified
+               uncomment below and update the code to test
+                //instance.action(function(error) {
+                //  if (error) throw error;
+                //expect().to.be();
+                //});
+                done();
+               */
+})

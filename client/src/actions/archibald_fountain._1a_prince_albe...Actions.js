d600db72ecd51e4148d93bda088193be09b7906e
Elly <elly@mg.crane.ai>
2018-11-03 17:43:41 -0400

    added client/src/actions/archibald_fountain refactoring

---
+++ client/src/actions/archibald_fountain._1a_prince_albe...Actions.js
@@ -0,0 +1,5 @@
+/*
+                This will control the networking for the archibald fountain. 1a prince albe... network requests
+            */
+import axios from "axios"
+import { client, url, setToken } from "../utils/axios.config.js"
